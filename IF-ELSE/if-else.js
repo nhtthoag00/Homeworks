259d1ea3db8be525a14bd881592db3d703632e78
nhtthoag00 <47780417+nhtthoag00@users.noreply.github.com>
2019-04-03 20:49:01 +0700

    add new file

--- IF-ELSE/if-else.js
+++ IF-ELSE/if-else.js
@@ -1,5 +1,5 @@
 var inputValue = prompt('What is the “official” name of JavaScript ?','');
-if (inputValue == "ECMAScript") {
+if (inputValue.trim() == "ECMAScript") {
     alert("Right !!");
 } else {
     alert("Didn’t know? ECMAScript !!");
