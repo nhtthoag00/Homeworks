cd4982a98204a104c7ca1971846e08efed8ff58b
nhtthoag00 <47780417+nhtthoag00@users.noreply.github.com>
2019-04-03 09:10:39 +0700

    add new file

---
+++ IF-ELSE/if-else.js
@@ -0,0 +1,6 @@
+var inputValue = prompt('What is the “official” name of JavaScript ?','');
+if (inputValue == "ECMAScript") {
+    alert("Right !!");
+} else {
+    alert("Didn’t know? ECMAScript !!");
+}
